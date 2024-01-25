ea59d017aed0c669b4bf41cc29b56567bf4baded
boltzu <37539638+boltzu@users.noreply.github.com>
2024-01-25 05:40:24 +0000

    Adicionado o prettier e formatados

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Se você leu isso, da uma risadinha</h1>
+  return <h1>Se você leu isso, da uma risadinha</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
